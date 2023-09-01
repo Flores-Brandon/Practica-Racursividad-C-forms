340b722e5dc9e78c6e4020c1f03cacbba31b9486
Flores-Brandon <I22050339@monclova.tecnm.mx>
2023-08-31 23:33:39 -0600

    Agregar archivos de proyecto.

---
+++ Practica Racursividad C# forms/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Practica_Racursividad_C__forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListar = new System.Windows.Forms.Button();
+            this.listBoxArchivos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(39, 125);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(120, 132);
+            this.btnListar.TabIndex = 0;
+            this.btnListar.Text = "Mostrar archivos";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // listBoxArchivos
+            // 
+            this.listBoxArchivos.FormattingEnabled = true;
+            this.listBoxArchivos.ItemHeight = 16;
+            this.listBoxArchivos.Location = new System.Drawing.Point(305, 12);
+            this.listBoxArchivos.Name = "listBoxArchivos";
+            this.listBoxArchivos.Size = new System.Drawing.Size(315, 356);
+            this.listBoxArchivos.TabIndex = 1;
+            this.listBoxArchivos.SelectedIndexChanged += new System.EventHandler(this.listBoxArchivos_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxArchivos);
+            this.Controls.Add(this.btnListar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.ListBox listBoxArchivos;
+    }
+}
+
---
+++ Practica Racursividad C# forms/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_Racursividad_C__forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            // Limpiar la lista antes de comenzar una nueva búsqueda
+            listBoxArchivos.Items.Clear();
+
+            string escritorioPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            ListarArchivosRecursivamente(escritorioPath);
+        }
+        private void ListarArchivosRecursivamente(string directorio)
+        {
+            try
+            {
+                // Obtener la lista de archivos en el directorio actual
+                string[] archivos = Directory.GetFiles(directorio);
+
+                // Agregar los archivos a la lista
+                foreach (string archivo in archivos)
+                {
+                    listBoxArchivos.Items.Add(Path.GetFileName(archivo) + " (" + Path.GetExtension(archivo) + ")");
+                }
+
+                // Recursivamente listar archivos en subdirectorios
+                string[] subdirectorios = Directory.GetDirectories(directorio);
+                foreach (string subdirectorio in subdirectorios)
+                {
+                    ListarArchivosRecursivamente(subdirectorio);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejo de errores si es necesario
+                listBoxArchivos.Items.Add("Error al listar archivos: " + ex.Message);
+            }
+        }
+
+        private void listBoxArchivos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+    
